8983c0b0f1ec764fab6f301fad46d50b003aa5ff
Fatima Abid <famkab2@gmail.com>
2023-10-27 20:13:01 +0200

    Changes to the look

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// You can add interactive elements using JavaScript here.
-// For example:
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Portfolio website loaded successfully.');
-});
+// Any interactive functionality goes here
