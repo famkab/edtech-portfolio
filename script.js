e151610280517478cad96312a068ba39fe628502
Fatima Abid <famkab2@gmail.com>
2023-10-27 19:59:49 +0200

    HTML/CSS/JS for Portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+// You can add interactive elements using JavaScript here.
+// For example:
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Portfolio website loaded successfully.');
+});
