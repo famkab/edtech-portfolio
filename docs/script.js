5e0ba47602654c6527ac6ff4908958352dda40e3
Fatima Abid <famkab2@gmail.com>
2023-11-08 19:27:18 +0100

    Changes to portfolio look

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,28 @@
-// Any interactive functionality goes here
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+// Active state in navigation
+window.addEventListener('scroll', event => {
+  let navigationLinks = document.querySelectorAll('nav ul li a');
+  let fromTop = window.scrollY;
+
+  navigationLinks.forEach(link => {
+      let section = document.querySelector(link.hash);
+
+      if (
+          section.offsetTop <= fromTop &&
+          section.offsetTop + section.offsetHeight > fromTop
+      ) {
+          link.classList.add('active');
+      } else {
+          link.classList.remove('active');
+      }
+  });
+});
